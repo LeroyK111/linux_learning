d8b006b1ae70dab19890d45bd32dc8e88b4e1b15
LeroyK <bridieiybnf@gmail.com>
2023-11-20 12:56:26 +0800

    1

--- initDNS.bat
+++ initDNS.bat
@@ -9,4 +9,10 @@ netsh interface ip add dns name = "ÒÔÌ«Íø 2" addr = 8.8.8.8
 netsh interface ip add dns name = "ÒÔÌ«Íø 2" addr = 8.8.4.4
 netsh interface ip add dns name = "ÒÔÌ«Íø 2" addr = 1.0.0.1
 netsh interface ip add dns name = "ÒÔÌ«Íø 2" addr = 1.1.1.1
+netsh interface ip add dns name = "WLAN" addr = 162.252.172.57
+netsh interface ip add dns name = "WLAN" addr = 149.154.159.92
+netsh interface ip add dns name = "WLAN" addr = 8.8.8.8
+netsh interface ip add dns name = "WLAN" addr = 8.8.4.4
+netsh interface ip add dns name = "WLAN" addr = 1.0.0.1
+netsh interface ip add dns name = "WLAN" addr = 1.1.1.1
 @REM pause
