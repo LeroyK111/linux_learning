fd696d13e14bae419fa071abe42f305b2caaf313
LeroyK <bridieiybnf@gmail.com>
2024-01-21 18:38:20 +0800

    1

--- initDNS.bat
+++ initDNS.bat
@@ -3,12 +3,12 @@ ipconfig /flushdns
 :: ĞŞ¸Ä ip mask getaway
 :: netsh interface ip set address "ÒÔÌ«Íø 2" static 10.102.220.223 255.255.254.0 10.102.220.1
 :: ĞŞ¸Ä dns
-netsh interface ip add dns name = "ÒÔÌ«Íø 2" addr = 162.252.172.57
-netsh interface ip add dns name = "ÒÔÌ«Íø 2" addr = 149.154.159.92
-netsh interface ip add dns name = "ÒÔÌ«Íø 2" addr = 8.8.8.8
-netsh interface ip add dns name = "ÒÔÌ«Íø 2" addr = 8.8.4.4
-netsh interface ip add dns name = "ÒÔÌ«Íø 2" addr = 1.0.0.1
-netsh interface ip add dns name = "ÒÔÌ«Íø 2" addr = 1.1.1.1
+netsh interface ip add dns name = "ÒÔÌ«Íø" addr = 162.252.172.57
+netsh interface ip add dns name = "ÒÔÌ«Íø" addr = 149.154.159.92
+netsh interface ip add dns name = "ÒÔÌ«Íø" addr = 8.8.8.8
+netsh interface ip add dns name = "ÒÔÌ«Íø" addr = 8.8.4.4
+netsh interface ip add dns name = "ÒÔÌ«Íø" addr = 1.0.0.1
+netsh interface ip add dns name = "ÒÔÌ«Íø" addr = 1.1.1.1
 netsh interface ip add dns name = "WLAN" addr = 162.252.172.57
 netsh interface ip add dns name = "WLAN" addr = 149.154.159.92
 netsh interface ip add dns name = "WLAN" addr = 8.8.8.8
